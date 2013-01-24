658ca10c1fbcbb9a43ead0e471a6821510a90a71
Darryl Sokoloski <dsokoloski@clearfoundation.com>
2013-01-24 19:08:45 +0000

    - Minor documentation / formatting changes.

--- deploy/libqos.lua
+++ deploy/libqos.lua
@@ -7,7 +7,7 @@
 --
 ------------------------------------------------------------------------------
 --
--- Copyright (C) 2012 ClearFoundation
+-- Copyright (C) 2012-2013 ClearFoundation
 -- 
 -- This program is free software; you can redistribute it and/or
 -- modify it under the terms of the GNU General Public License
