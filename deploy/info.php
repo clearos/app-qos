ce00eba599941deec4b8eef26929a13f5e153565
pcbaldwin <pbaldwin@clearfoundation.com>
2014-09-03 00:51:32 -0400

    Creating ClearOS6 branch and tag

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'qos';
-$app['version'] = '1.5.22';
+$app['version'] = '1.6.5';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-qos.spec
+++ packaging/app-qos.spec
@@ -1,7 +1,7 @@
 
 Name: app-qos
 Epoch: 1
-Version: 1.5.22
+Version: 1.6.5
 Release: 1%{dist}
 Summary: QoS
 License: GPLv3
