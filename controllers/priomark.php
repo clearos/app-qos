d434329471f03b6e26b83374b557fbca0b6b6907
pcbaldwin <peter@egloo.ca>
2016-07-14 13:35:39 -0400

    Spacing/lint

--- controllers/priomark.php
+++ controllers/priomark.php
@@ -282,27 +282,11 @@ class Priomark extends ClearOS_Controller
         // Set validation rules
         //---------------------
 
-        $this->form_validation->set_policy(
-            'nickname', 'qos/Qos',
-            'validate_nickname', TRUE
-        );
-        $this->form_validation->set_policy(
-            'saddr', 'qos/Qos',
-            'validate_address', FALSE
-        );
-        $this->form_validation->set_policy(
-            'sport', 'qos/Qos',
-            'validate_port', FALSE
-        );
-        $this->form_validation->set_policy(
-            'daddr', 'qos/Qos',
-            'validate_address', FALSE
-        );
-        $this->form_validation->set_policy(
-            'dport', 'qos/Qos',
-            'validate_port', FALSE
-        );
-
+        $this->form_validation->set_policy('nickname', 'qos/Qos', 'validate_nickname', TRUE);
+        $this->form_validation->set_policy('saddr', 'qos/Qos','validate_address', FALSE);
+        $this->form_validation->set_policy('sport', 'qos/Qos', 'validate_port', FALSE);
+        $this->form_validation->set_policy('daddr', 'qos/Qos', 'validate_address', FALSE);
+        $this->form_validation->set_policy('dport', 'qos/Qos', 'validate_port', FALSE);
         $this->form_validation->set_policy('interface', 'qos/Qos', 'validate_interface', FALSE);
         $this->form_validation->set_policy('enabled', 'qos/Qos', 'validate_state', FALSE);
         $this->form_validation->set_policy('priority', 'qos/Qos', 'validate_priority', TRUE);
