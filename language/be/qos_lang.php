aecd3f8d5f6e3acc1a2cd07ad3a4193941cabc87
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:15:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/qos_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['qos_add_priomark_downstream_rule'] = 'Дадаць правіла Класс Downstream Priority';
+$lang['qos_add_priomark_upstream_rule'] = 'Дадаць Upstream правілы прыярытэту класа';
+$lang['qos_any'] = 'любы';
+$lang['qos_app_description'] = 'Дадатак менеджэр паласы прапускання і QoS гэта сеткавая функцыя, якая дазваляе адміністратарам вызначыць прыярытэтнасць пэўных тыпаў інтэрнэт-трафіку. Ўключэнне QoS памяншае верагоднасць таго, што ў любы момант часу, адзін карыстальнік або прылада можа аказваць неспрыяльнае ўздзеянне на прадукцыйнасць сеткі насычэннем даступную паласу прапускання.';
+$lang['qos_app_name'] = 'Паласа прапускання і QoS менеджэр';
+$lang['qos_destination'] = 'прызначэння';
+$lang['qos_direction'] = 'напрамак';
+$lang['qos_edit_priomark_downstream_rule'] = 'Рэдагаванне правілы класа прыярытэту Уніз па плыні';
+$lang['qos_edit_priomark_upstream_rule'] = 'Рэдагаванне Upstream правілы прыярытэту класа';
+$lang['qos_enable_all'] = 'ўключыць усе';
+$lang['qos_engine_disable'] = 'адключыць рухавіка';
+$lang['qos_engine_enable'] = 'ўключыць рухавік';
+$lang['qos_equalize'] = 'зраўнаваць';
+$lang['qos_interface_add_title'] = 'Дадаць канфігурацыю вонкавага інтэрфейсу';
+$lang['qos_interface_edit_title'] = 'Канфігурацыя інтэрфейсу Рэдагаваць Знешні';
+$lang['qos_interface_speed_not_set'] = 'Існуе альбо не ўверх па плыні ці ўніз па плыні набор для гэтага інтэрфейсу. Калі ласка, запусціць тэст хуткасці на інтэрфейсе перад даданнем яго да рухавіка QoS.';
+$lang['qos_invalid_direction'] = 'Invalid кірунак.';
+$lang['qos_invalid_limit_value'] = 'Invalid лімітавае значэнне.';
+$lang['qos_invalid_r2q'] = 'Няправільнае значэнне R2q.';
+$lang['qos_invalid_reservation_value'] = 'Invalid правілы браніравання.';
+$lang['qos_name'] = 'імя';
+$lang['qos_not'] = 'ня';
+$lang['qos_prioclass_limit_underflow'] = 'Гранічны клас прыярытэту не можа быць усталяваны на значэнне, меншае, чым адпаведнае рэзерваванне паласы прапускання. Звярніцеся да кіраўніцтва карыстальніка для атрымання дадатковай інфармацыі. Мяжа згушчанага выяўлена класа прыярытэту #';
+$lang['qos_priomark_downstream_rules'] = 'Правілы класа Downstream Priority';
+$lang['qos_priomark_invalid_interface'] = 'Няправільны або несконфигурированным знешні інтэрфейс.';
+$lang['qos_priomark_not_found'] = 'Правіла клас прыярытэту не знойдзены';
+$lang['qos_priomark_upstream_rules'] = 'Правілы класа Upstream Прыярытэтныя';
+$lang['qos_priority'] = 'прыярытэт';
+$lang['qos_ramp'] = 'пахіл';
+$lang['qos_rate_to_quantum_auto'] = 'Аўтаматычны Rate да Quantum';
+$lang['qos_rate_to_quantum'] = 'Хуткасць да Quantum';
+$lang['qos_reset'] = 'скід';
+$lang['qos_source'] = 'крыніца';
+$lang['qos_protocol_must_be_specified_using_ports'] = 'Пратакол павінен быць усталяваны TCP або UDP пры выкарыстанні партоў.';
