c0b930284c529cb0c2a73d53ce7351501cf22e5f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:26:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/qos_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['qos_add_priomark_downstream_rule'] = 'Иловаи афзалиятнок дар поёноб Синфи Ќоидаи';
+$lang['qos_add_priomark_upstream_rule'] = 'Иловаи афзалиятнок дар болооби Синфи Ќоидаи';
+$lang['qos_any'] = 'ҳар як';
+$lang['qos_app_description'] = 'Дар барнома менеҷери Зарфият ва QoS хусусияти шабакавие аст, ки имкон медиҳад, маъмурони барои авлавият баъзе намудҳои трафики Интернет аст. Даргиронидани QoS эҳтимоли, ки дар вақти дилхоҳ дода, дар як истифодабаранда ё дастгоҳи ягонаи то иҷрои шабака аз тарафи saturating маҷрои дастрас хор коҳиш меёбад.';
+$lang['qos_app_name'] = 'Зарфият ва QoS Manager';
+$lang['qos_destination'] = 'Самт';
+$lang['qos_direction'] = 'равона';
+$lang['qos_edit_priomark_downstream_rule'] = 'Таҳрир афзалиятнок дар поёноб Синфи Ќоидаи';
+$lang['qos_edit_priomark_upstream_rule'] = 'Таҳрир афзалиятнок дар болооби Синфи Ќоидаи';
+$lang['qos_enable_all'] = 'Даргиронидани Ҳамаи';
+$lang['qos_engine_disable'] = 'Engine хомӯш';
+$lang['qos_engine_enable'] = 'Даргиронидани Engine';
+$lang['qos_equalize'] = 'баробар';
+$lang['qos_interface_add_title'] = 'Иловаи беруна Батанзимдарорӣ интерфейс';
+$lang['qos_interface_edit_title'] = 'Таҳрири Батанзимдарорӣ интерфейс беруна';
+$lang['qos_interface_speed_not_set'] = 'аст ё не маҷмӯи болооб ва ё поёноб барои ин интерфейси нест. Лутфан як санҷиши суръат оид ба интерфейси пеш илова ба муҳаррики QoS давида.';
+$lang['qos_invalid_direction'] = 'самти нодуруст аст.';
+$lang['qos_invalid_limit_value'] = 'арзиши лимити нодуруст аст.';
+$lang['qos_invalid_r2q'] = 'арзиши r2q нодуруст аст.';
+$lang['qos_invalid_reservation_value'] = 'волоияти фармоиш нодуруст аст.';
+$lang['qos_name'] = 'ном';
+$lang['qos_not'] = 'не';
+$lang['qos_prioclass_limit_underflow'] = 'Меъёри синфи афзалиятноки метавонад ба арзиши камтар аз қайду маҷрои дахлдор карда намешавад муқаррар карда мешавад. Дастур корбар барои маълумоти бештар нигаред. underflow Ҳадди ошкор барои синфи афзалиятноки #';
+$lang['qos_priomark_downstream_rules'] = 'Қоидаҳои Синфи афзалиятнок поѐноб';
+$lang['qos_priomark_invalid_interface'] = 'Интерфейси беруна номӯътабар ё unconfigured.';
+$lang['qos_priomark_not_found'] = 'волоияти синфи Афзалияти ёфт нашуд';
+$lang['qos_priomark_upstream_rules'] = 'Қоидаҳои Синфи афзалиятнок болооб';
+$lang['qos_priority'] = 'Афзалияти';
+$lang['qos_ramp'] = 'чарх';
+$lang['qos_rate_to_quantum_auto'] = 'Худкори Меъёри-ба-квантӣ';
+$lang['qos_rate_to_quantum'] = 'Меъёри-ба-квантӣ';
+$lang['qos_reset'] = 'Reset';
+$lang['qos_source'] = 'сарчашма';
+$lang['qos_protocol_must_be_specified_using_ports'] = 'Протокол бояд ба TCP ё UDP, агар бо истифода аз бандарҳои муқаррар карда мешавад.';
