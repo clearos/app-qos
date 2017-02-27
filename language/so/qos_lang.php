e8c28ffa1d79d73da0dae16160a774f14f9df553
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:25:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/qos_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['qos_add_priomark_downstream_rule'] = 'Add Mudnaanta Downstream Rule Class';
+$lang['qos_add_priomark_upstream_rule'] = 'Add Mudnaanta kheyraad Rule Class';
+$lang['qos_any'] = 'Qof kasta oo';
+$lang['qos_app_description'] = 'The bandwidth iyo QoS app Manager waa feature a network ogolaanaya maamulayaasha in ay mudnaanta siiyaan noocyo ka mid ah gaadiidka Internet. Fullintu QoS hoos suurtagalnimada in mar kasta la siiyaa, hal user ama qalab xaqiro laga yaabaa in qaab network by saturating bandwidth heli karo.';
+$lang['qos_app_name'] = 'Bandwidth iyo Manager QoS';
+$lang['qos_destination'] = 'Destination';
+$lang['qos_direction'] = 'Direction';
+$lang['qos_edit_priomark_downstream_rule'] = 'Edit Mudnaanta Downstream Rule Class';
+$lang['qos_edit_priomark_upstream_rule'] = 'Edit Mudnaanta kheyraad Rule Class';
+$lang['qos_enable_all'] = 'sahlo All';
+$lang['qos_engine_disable'] = 'Engine Disable';
+$lang['qos_engine_enable'] = 'sahlo Engine';
+$lang['qos_equalize'] = 'barbaraha';
+$lang['qos_interface_add_title'] = 'Add Dibadda Wajahadda haggaajin';
+$lang['qos_interface_edit_title'] = 'Edit Dibadda Wajahadda haggaajin';
+$lang['qos_interface_speed_not_set'] = 'Waxaa sidoo kale aan kheyraad ama hooseeya set for interface this. Fadlan ordi Test Speed ​​on interface ah ka hor inta ku daray in engine QoS ah.';
+$lang['qos_invalid_direction'] = 'jihada aan sax ahayn.';
+$lang['qos_invalid_limit_value'] = 'qiimaha xadka aan sax ahayn.';
+$lang['qos_invalid_r2q'] = 'qiimaha r2q aan sax ahayn.';
+$lang['qos_invalid_reservation_value'] = 'qaanuunka boos aan sax ahayn.';
+$lang['qos_name'] = 'Magaca';
+$lang['qos_not'] = 'Ma aha';
+$lang['qos_prioclass_limit_underflow'] = 'xadka fasalka mudnaanta A oo aan la hagaajin karo in ay qiimo ka yar Boos bandwidth baahisey. Eeg Tusaha User u hesho macluumaad dheeraad ah. Yaree underflow ogaado fasalka mudnaanta #';
+$lang['qos_priomark_downstream_rules'] = 'Xeerarka Class Mudnaanta Downstream';
+$lang['qos_priomark_invalid_interface'] = 'Aan sax ahayn ama unconfigured interface dibadda.';
+$lang['qos_priomark_not_found'] = 'Mudnaanta xukunka fasalka ma aanan helin';
+$lang['qos_priomark_upstream_rules'] = 'Xeerarka Class Mudnaanta kheyraad';
+$lang['qos_priority'] = 'Mudnaanta';
+$lang['qos_ramp'] = 'ramp';
+$lang['qos_rate_to_quantum_auto'] = 'Automatic Rate-to-Quantum';
+$lang['qos_rate_to_quantum'] = 'Rate-to-Quantum';
+$lang['qos_reset'] = 'Dib u celi';
+$lang['qos_source'] = 'Source';
+$lang['qos_protocol_must_be_specified_using_ports'] = 'Protocol waa in la dhigay si ay u TCP ama UDP haddii la isticmaalayo dekadaha.';
