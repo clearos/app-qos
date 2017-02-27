5f69c357cf3cca5b8aa6b60567cc5fad685b936e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:15:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/qos_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['qos_add_priomark_downstream_rule'] = 'Gehitu Downstream Priority Class Araua';
+$lang['qos_add_priomark_upstream_rule'] = 'Gehitu Upstream Priority Class Araua';
+$lang['qos_any'] = 'Edozein';
+$lang['qos_app_description'] = 'The Bytes eta QoS kudeatzailea aplikazioa sareko funtzio bat ahalbidetzen administratzaileak Internet trafiko-mota batzuk lehenesteko da. QoS gaitzea aukera hori edozein momentutan, erabiltzaileak edo gailu bakar bat sare performance degradatzen liteke eskuragarri banda zabalera saturatzeko jaitsi da.';
+$lang['qos_app_name'] = 'Bytes eta QoS Manager';
+$lang['qos_destination'] = 'Helmuga';
+$lang['qos_direction'] = 'Zuzendaritza';
+$lang['qos_edit_priomark_downstream_rule'] = 'Editatu Downstream Priority Class Araua';
+$lang['qos_edit_priomark_upstream_rule'] = 'Editatu Upstream Priority Class Araua';
+$lang['qos_enable_all'] = 'Gaitu guztiak';
+$lang['qos_engine_disable'] = 'Disable Engine';
+$lang['qos_engine_enable'] = 'Gaitu Engine';
+$lang['qos_equalize'] = 'berdindu';
+$lang['qos_interface_add_title'] = 'Gehitu Kanpo interfazearen konfigurazioa';
+$lang['qos_interface_edit_title'] = 'Editatu kanpoko interfazearen konfigurazioa';
+$lang['qos_interface_speed_not_set'] = 'Badira bai, ez upstream edo downstream interface horretarako zehaztu da. Mesedez, exekutatu Abiadura interfazea Test bat gehituz QoS motorra aurretik.';
+$lang['qos_invalid_direction'] = 'norabide okerra.';
+$lang['qos_invalid_limit_value'] = 'muga balioa okerra da.';
+$lang['qos_invalid_r2q'] = 'r2q balio.';
+$lang['qos_invalid_reservation_value'] = 'Erreserba arau baliogabea.';
+$lang['qos_name'] = 'izena';
+$lang['qos_not'] = 'Ez';
+$lang['qos_prioclass_limit_underflow'] = 'lehentasuna class muga bat ezin da balio bat dagokion banda zabalera erreserba baino gutxiago ezarri. Ikusi erabiltzailearen gida informazio gehiagorako. Mugatu gutxiegikorik lehentasuna class # detektatu';
+$lang['qos_priomark_downstream_rules'] = 'Downstream Priority Class arauak';
+$lang['qos_priomark_invalid_interface'] = 'kanpoko interfazea baliogabea edo konfiguratu gabe.';
+$lang['qos_priomark_not_found'] = 'Lehentasuna class araua ez da aurkitu';
+$lang['qos_priomark_upstream_rules'] = 'Upstream Priority Class arauak';
+$lang['qos_priority'] = 'Lehentasuna';
+$lang['qos_ramp'] = 'Ramp';
+$lang['qos_rate_to_quantum_auto'] = 'Automatikoa Rate-to-Quantum';
+$lang['qos_rate_to_quantum'] = 'Tarifa-to-Quantum';
+$lang['qos_reset'] = 'Berrezarri';
+$lang['qos_source'] = 'Iturria';
+$lang['qos_protocol_must_be_specified_using_ports'] = 'Protokoloa egon TCP edo UDP ezarri behar portuak erabiliz gero.';
