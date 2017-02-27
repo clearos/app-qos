c6a7dc8cb276581101891ba483a4753ac260dc33
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:25:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/qos_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['qos_add_priomark_downstream_rule'] = 'Kuongeza Mto kipaumbele Hatari Rule';
+$lang['qos_add_priomark_upstream_rule'] = 'Kuongeza Upstream kipaumbele Hatari Rule';
+$lang['qos_any'] = 'yoyote';
+$lang['qos_app_description'] = 'Bandwidth na QoS programu Meneja ni mtandao kipengele kwamba inaruhusu watendaji kuweka kipaumbele katika aina fulani ya trafiki mtandao. Kuwezesha QoS itapungua uwezekano kwamba wakati wowote, mtumiaji moja au kifaa inaweza kuharibu utendaji wa mtandao kwa kueneza inapatikana Bandwidth.';
+$lang['qos_app_name'] = 'Bandwidth na QoS Meneja';
+$lang['qos_destination'] = 'marudio';
+$lang['qos_direction'] = 'mwelekeo';
+$lang['qos_edit_priomark_downstream_rule'] = 'Hariri Mto kipaumbele Hatari Rule';
+$lang['qos_edit_priomark_upstream_rule'] = 'Hariri Upstream kipaumbele Hatari Rule';
+$lang['qos_enable_all'] = 'kuwawezesha All';
+$lang['qos_engine_disable'] = 'Disable injini za';
+$lang['qos_engine_enable'] = 'kuwawezesha injini za';
+$lang['qos_equalize'] = 'kusawazisha';
+$lang['qos_interface_add_title'] = 'Kuongeza External Interface Configuration';
+$lang['qos_interface_edit_title'] = 'Edit External Interface Configuration';
+$lang['qos_interface_speed_not_set'] = 'Kuna ama si mto au mto kuweka kwa interface hii. Tafadhali kukimbia mtihani kasi juu ya interface kabla ya kuongeza kwa injini QoS.';
+$lang['qos_invalid_direction'] = 'mwelekeo batili.';
+$lang['qos_invalid_limit_value'] = 'thamani batili kikomo.';
+$lang['qos_invalid_r2q'] = 'thamani batili r2q.';
+$lang['qos_invalid_reservation_value'] = 'Batili reservation utawala.';
+$lang['qos_name'] = 'jina';
+$lang['qos_not'] = 'si';
+$lang['qos_prioclass_limit_underflow'] = 'kikomo kipaumbele darasa haiwezi kuweka kwa thamani chini ya reservation sambamba Bandwidth. Angalia Mwongozo wa Mtumiaji kwa maelezo zaidi. Kikomo underflow wanaona kwa kipaumbele darasa #';
+$lang['qos_priomark_downstream_rules'] = 'Mto kipaumbele Hatari Kanuni';
+$lang['qos_priomark_invalid_interface'] = 'Batili au kama haijasanidiwa interface nje.';
+$lang['qos_priomark_not_found'] = 'Kipaumbele darasa utawala hakuonekana';
+$lang['qos_priomark_upstream_rules'] = 'Katika chanzo cha maji kipaumbele Hatari Kanuni';
+$lang['qos_priority'] = 'kipaumbele';
+$lang['qos_ramp'] = 'njia panda';
+$lang['qos_rate_to_quantum_auto'] = 'Moja kwa moja Kiwango cha-to-Quantum';
+$lang['qos_rate_to_quantum'] = 'Kiwango cha-to-Quantum';
+$lang['qos_reset'] = 'Rudisha';
+$lang['qos_source'] = 'chanzo';
+$lang['qos_protocol_must_be_specified_using_ports'] = 'Itifaki lazima kuweka TCP au UDP kama kutumia bandari.';
