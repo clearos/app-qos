b1ae95c1d3e0b13c70dd475cc20da5d7f9fbe435
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:27:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/qos_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['qos_add_priomark_downstream_rule'] = 'Ychwanegu Rheol Dosbarth Blaenoriaeth Downstream';
+$lang['qos_add_priomark_upstream_rule'] = 'Ychwanegwch fyny&#39;r afon Rheol Dosbarth Blaenoriaeth';
+$lang['qos_any'] = 'unrhyw';
+$lang['qos_app_description'] = 'Rheolwr lled band a QoS app yn nodwedd rhwydwaith sy&#39;n caniatáu i weinyddwyr i flaenoriaethu rhai mathau o draffig Rhyngrwyd. Galluogi QoS yn lleihau&#39;r tebygolrwydd y ar unrhyw adeg benodol, gallai defnyddiwr unigol neu ddyfais ddiraddio perfformiad y rhwydwaith drwy saturating lled band sydd ar gael.';
+$lang['qos_app_name'] = 'Rheolwr QoS Bandwidth a';
+$lang['qos_destination'] = 'cyrchfan';
+$lang['qos_direction'] = 'cyfarwyddyd';
+$lang['qos_edit_priomark_downstream_rule'] = 'Golygu Rheol Dosbarth Blaenoriaeth Downstream';
+$lang['qos_edit_priomark_upstream_rule'] = 'Golygu fyny&#39;r afon Rheol Dosbarth Blaenoriaeth';
+$lang['qos_enable_all'] = 'galluogi pob';
+$lang['qos_engine_disable'] = 'Engine Analluogi';
+$lang['qos_engine_enable'] = 'galluogi Engine';
+$lang['qos_equalize'] = 'gydraddoli&#39;r';
+$lang['qos_interface_add_title'] = 'Ychwanegu Rhyngwyneb Allanol Ffurfweddiad';
+$lang['qos_interface_edit_title'] = 'Golygu Allanol Rhyngwyneb Ffurfweddiad';
+$lang['qos_interface_speed_not_set'] = 'Mae naill ai set i fyny neu i lawr yr afon ar gyfer y rhyngwyneb hwn. Os gwelwch yn dda rhedeg Prawf Cyflymder ar y rhyngwyneb cyn ei ychwanegu at y peiriant QoS.';
+$lang['qos_invalid_direction'] = 'cyfeiriad annilys.';
+$lang['qos_invalid_limit_value'] = 'Gwerth terfyn annilys.';
+$lang['qos_invalid_r2q'] = 'gwerth r2q annilys.';
+$lang['qos_invalid_reservation_value'] = 'rheol llain annilys.';
+$lang['qos_name'] = 'enw';
+$lang['qos_not'] = 'Ddim';
+$lang['qos_prioclass_limit_underflow'] = 'Ni all cyfyngiad dosbarth blaenoriaeth gael ei osod i&#39;r gwerth yn llai na&#39;r llain lled band cyfatebol. Gweler y Canllaw Defnyddwyr am ragor o wybodaeth. islif Terfyn canfod ar gyfer y dosbarth blaenoriaeth #';
+$lang['qos_priomark_downstream_rules'] = 'Rheolau Dosbarth Blaenoriaeth Downstream';
+$lang['qos_priomark_invalid_interface'] = 'rhyngwyneb allanol annilys neu&#39;n heb ei gyflunio.';
+$lang['qos_priomark_not_found'] = 'rheol dosbarth Blaenoriaeth heb ei ganfod';
+$lang['qos_priomark_upstream_rules'] = 'Rheolau Dosbarth Blaenoriaeth i fyny&#39;r afon';
+$lang['qos_priority'] = 'blaenoriaeth';
+$lang['qos_ramp'] = 'ramp';
+$lang['qos_rate_to_quantum_auto'] = 'Awtomatig Cyfradd-i-Quantum';
+$lang['qos_rate_to_quantum'] = 'Cyfradd-i-Quantum';
+$lang['qos_reset'] = 'Ailosod';
+$lang['qos_source'] = 'ffynhonnell';
+$lang['qos_protocol_must_be_specified_using_ports'] = 'Rhaid Protocol eu gosod i TCP neu CDU os ydych yn defnyddio porthladdoedd.';
