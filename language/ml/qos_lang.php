54a017fe89fa1bae16172b069720b8bf5a76debc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:22:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/qos_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['qos_add_priomark_downstream_rule'] = 'ഡൗൺസ്ട്രീം മുൻഗണന ക്ലാസ്സ് നിയമം ചേർക്കുക';
+$lang['qos_add_priomark_upstream_rule'] = 'അപ്സ്ട്രീം മുൻഗണന ക്ലാസ്സ് നിയമം ചേർക്കുക';
+$lang['qos_any'] = 'എന്തെങ്കിലും';
+$lang['qos_app_description'] = 'ബാൻഡ്വിഡ്ത്തിനും QoS മാനേജർ അപ്ലിക്കേഷൻ അഡ്മിനിസ്ട്രേറ്റർമാർ ഇന്റർനെറ്റ് ട്രാഫിക് പ്രത്യേക തരം മുൻഗണന അനുവദിക്കുന്ന ഒരു നെറ്റ്വർക്ക് സവിശേഷതയാണ്. QoS പ്രാപ്തമാക്കുന്നത് ഏതൊരു സമയത്തും, ഒരു സിംഗിൾ യൂസർ അല്ലെങ്കിൽ ഉപകരണം ലഭ്യമാണ് ബാൻഡ്വിഡ്ത്ത് വികാസവും നെറ്റ്വർക്ക് പെർഫോമൻസ് ഡീഗ്രേഡ് തക്കവണ്ണം സാധ്യത കുറയ്ക്കുന്നു.';
+$lang['qos_app_name'] = 'ബാൻഡ്വിഡ്ത്തിനും QoS മാനേജർ';
+$lang['qos_destination'] = 'ലക്ഷ്യസ്ഥാനം';
+$lang['qos_direction'] = 'സംവിധാനം';
+$lang['qos_edit_priomark_downstream_rule'] = 'ഡൗൺസ്ട്രീം മുൻഗണന ക്ലാസ്സ് നിയമം എഡിറ്റ്';
+$lang['qos_edit_priomark_upstream_rule'] = 'അപ്സ്ട്രീം മുൻഗണന ക്ലാസ്സ് നിയമം എഡിറ്റ്';
+$lang['qos_enable_all'] = 'എല്ലാ പ്രാപ്തമാക്കുക';
+$lang['qos_engine_disable'] = 'എഞ്ചിൻ അപ്രാപ്തമാക്കുക';
+$lang['qos_engine_enable'] = 'എഞ്ചിൻ പ്രാപ്തമാക്കുക';
+$lang['qos_equalize'] = 'തുല്യമായരീതിയിൽ';
+$lang['qos_interface_add_title'] = 'ബാഹ്യ ഇന്റർഫെയിസ് ക്രമീകരണ ചേർക്കുക';
+$lang['qos_interface_edit_title'] = 'എഡിറ്റ് ബാഹ്യ ഇന്റർഫെയിസ് ക്രമീകരണ';
+$lang['qos_interface_speed_not_set'] = 'ഈ ഇന്റർഫെയിസിനുള്ള ഒന്നുകിൽ അപ്സ്ട്രീം അല്ലെങ്കിൽ അധികജലം അല്ല സെറ്റ് ഇല്ല. QoS എഞ്ചിൻ ചേർത്ത് മുമ്പ് ഇന്റർഫേസ് ഒരു സ്പീഡ് ടെസ്റ്റ് പ്രവർത്തിപ്പിക്കുക.';
+$lang['qos_invalid_direction'] = 'അസാധുവായ ദിശ.';
+$lang['qos_invalid_limit_value'] = 'അസാധുവായ പരിധി മൂല്യം.';
+$lang['qos_invalid_r2q'] = 'അസാധുവായ r2q മൂല്യം.';
+$lang['qos_invalid_reservation_value'] = 'അസാധുവായ റിസർവേഷൻ റൂൾ.';
+$lang['qos_name'] = 'പേര്';
+$lang['qos_not'] = 'അല്ല';
+$lang['qos_prioclass_limit_underflow'] = 'മുൻഗണനാ ക്ലാസ് പരിധി അല്ല ഇതേ ബാൻഡ്വിഡ്ത്ത് സംവരണം കുറവ് ഒരു മൂല്യം ക്രമീകരിക്കാൻ കഴിയും. കൂടുതൽ വിവരങ്ങൾക്ക് ഉപയോക്തൃ ഗൈഡ് കാണുക. പരിധി underflow മുൻഗണന ക്ലാസ് # കണ്ടെത്തി';
+$lang['qos_priomark_downstream_rules'] = 'അധികജലം മുൻഗണന ക്ലാസ് നിയമങ്ങൾ';
+$lang['qos_priomark_invalid_interface'] = 'അസാധുവായ അല്ലെങ്കിൽ കോൺഫിഗർ ബാഹ്യ ഇന്റർഫേസ്.';
+$lang['qos_priomark_not_found'] = 'മുൻഗണന ക്ലാസ് നയം കണ്ടെത്തിയില്ല';
+$lang['qos_priomark_upstream_rules'] = 'അപ്സ്ട്രീം മുൻഗണന ക്ലാസ് നിയമങ്ങൾ';
+$lang['qos_priority'] = 'മുൻഗണന';
+$lang['qos_ramp'] = 'റാംപ്';
+$lang['qos_rate_to_quantum_auto'] = 'ഓട്ടോമാറ്റിക് നിരക്ക്-ടു-ക്വാണ്ടം';
+$lang['qos_rate_to_quantum'] = 'നിരക്ക്-ടു-ക്വാണ്ടം';
+$lang['qos_reset'] = 'റീസെറ്റ്';
+$lang['qos_source'] = 'ഉറവിടം';
+$lang['qos_protocol_must_be_specified_using_ports'] = 'പ്രോട്ടോക്കോൾ അര്ഥങ്ങളെപ്പറ്റിയും എങ്കിൽ TCP അല്ലെങ്കിൽ UDP ലേക്ക് സജ്ജമാക്കണം.';
