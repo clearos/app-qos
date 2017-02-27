7cd834350c6800f7eecd14fb206872357289b804
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:19:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/qos_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['qos_add_priomark_downstream_rule'] = 'Cuir Rang Riail Tosaíochta iartheachtach';
+$lang['qos_add_priomark_upstream_rule'] = 'Cuir In aghaidh srutha Rang Riail Tosaíochta';
+$lang['qos_any'] = 'aon';
+$lang['qos_app_description'] = 'Is é an Bainisteoir Bandaleithid agus QoS app gné líonra a ligeann riarthóirí tosaíocht a thabhairt cineálacha éagsúla tráchta Idirlíon. QoS Chumasú laghdaíonn an dóchúlacht go ag aon am ar leith, d&#39;fhéadfadh d&#39;úsáideoir amháin nó feiste degrade fheidhmíocht líonra trí saturating bandaleithead ar fáil.';
+$lang['qos_app_name'] = 'Bandaleithid agus Bainisteoir QoS';
+$lang['qos_destination'] = 'Scríbe';
+$lang['qos_direction'] = 'treo';
+$lang['qos_edit_priomark_downstream_rule'] = 'Athraigh an Aicme Riail Tosaíochta iartheachtach';
+$lang['qos_edit_priomark_upstream_rule'] = 'Athraigh In aghaidh srutha Rang Riail Tosaíochta';
+$lang['qos_enable_all'] = 'Cumasaigh uile';
+$lang['qos_engine_disable'] = 'Inneall Díchumasaigh';
+$lang['qos_engine_enable'] = 'Cumasaigh Inneall';
+$lang['qos_equalize'] = 'chomhionannú';
+$lang['qos_interface_add_title'] = 'Cuir Cumraíocht Comhéadan Seachtrach';
+$lang['qos_interface_edit_title'] = 'Cuir Cumraíocht Comhéadan Seachtrach';
+$lang['qos_interface_speed_not_set'] = 'Tá ceachtar ní leagtha réamhtheachtach nó iartheachtach don chomhéadan. Tabhair reáchtáil Speed ​​Test ar an gcomhéadan sular cuireadh é chuig an t-inneall QoS.';
+$lang['qos_invalid_direction'] = 'treo neamhbhailí.';
+$lang['qos_invalid_limit_value'] = 'teorainnluacha neamhbhailí.';
+$lang['qos_invalid_r2q'] = 'luach r2q neamhbhailí.';
+$lang['qos_invalid_reservation_value'] = 'riail áirithinte neamhbhailí.';
+$lang['qos_name'] = 'Ainm';
+$lang['qos_not'] = 'Nach bhfuil';
+$lang['qos_prioclass_limit_underflow'] = 'Ní féidir le teorainn rang tosaíochta a shocrú chun luach níos lú ná an áirithint bandaleithead comhfhreagrach. Féach ar an Treoir d&#39;Úsáideoirí le haghaidh tuilleadh eolais. underflow Teorainn braitheadh ​​do rang tosaíochta #';
+$lang['qos_priomark_downstream_rules'] = 'Rialacha Ranga Tosaíochta sruth';
+$lang['qos_priomark_invalid_interface'] = 'Neamhbhailí nó unconfigured comhéadan seachtracha.';
+$lang['qos_priomark_not_found'] = 'riail rang Tosaíochta gan aimsiú';
+$lang['qos_priomark_upstream_rules'] = 'Rialacha Ranga Tosaíochta In aghaidh srutha';
+$lang['qos_priority'] = 'tosaíocht';
+$lang['qos_ramp'] = 'rampa';
+$lang['qos_rate_to_quantum_auto'] = 'Uathoibríoch Ráta-go-Quantum';
+$lang['qos_rate_to_quantum'] = 'Ráta-go-Quantum';
+$lang['qos_reset'] = 'Athshocraigh';
+$lang['qos_source'] = 'Foinse';
+$lang['qos_protocol_must_be_specified_using_ports'] = 'Caithfear Prótacal a shocrú chun TCP nó UDP más ag baint úsáide calafoirt.';
