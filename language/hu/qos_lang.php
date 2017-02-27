d2bc8927cf0218e4f77b881164fbd0f2c513f1cd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:19:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/qos_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['qos_add_priomark_downstream_rule'] = 'Add Downstream Priority Class szabály';
+$lang['qos_add_priomark_upstream_rule'] = 'Add Upstream Priority Class szabály';
+$lang['qos_any'] = 'Bármilyen';
+$lang['qos_app_description'] = 'A sávszélesség és QoS kezelő alkalmazás egy hálózati szolgáltatás, amely lehetővé teszi a rendszergazdák előnyben bizonyos típusú internetes forgalmat. Engedélyezése QoS csökkenti annak valószínűségét, hogy az adott időpontban, egy adott felhasználó vagy eszköz ronthatja a hálózati teljesítményt leterhelni sávszélesség.';
+$lang['qos_app_name'] = 'Sávszélesség és QoS menedzser';
+$lang['qos_destination'] = 'Rendeltetési hely';
+$lang['qos_direction'] = 'Irány';
+$lang['qos_edit_priomark_downstream_rule'] = 'Szerkesztés Downstream Priority Class szabály';
+$lang['qos_edit_priomark_upstream_rule'] = 'Szerkesztés Upstream Priority Class szabály';
+$lang['qos_enable_all'] = 'Az összes engedélyezése';
+$lang['qos_engine_disable'] = 'letiltása Engine';
+$lang['qos_engine_enable'] = 'engedélyezze Engine';
+$lang['qos_equalize'] = 'kiegyenlít';
+$lang['qos_interface_add_title'] = 'Add Külső interfész konfigurációja';
+$lang['qos_interface_edit_title'] = 'Szerkesztés Külső interfész konfigurációja';
+$lang['qos_interface_speed_not_set'] = 'Vagy nincs upstream vagy downstream készlet ezt a felületet. Kérjük futtatni egy Speed ​​Test a felület, mielőtt hozzátéve, hogy a QoS motor.';
+$lang['qos_invalid_direction'] = 'Érvénytelen irányba.';
+$lang['qos_invalid_limit_value'] = 'Érvénytelen határértéket.';
+$lang['qos_invalid_r2q'] = 'Érvénytelen r2q érték.';
+$lang['qos_invalid_reservation_value'] = 'Érvénytelen foglalás szabályt.';
+$lang['qos_name'] = 'Név';
+$lang['qos_not'] = 'Nem';
+$lang['qos_prioclass_limit_underflow'] = 'A prioritási osztály határértéke nem lehet ennek az értéke kisebb, mint a megfelelő sávszélesség foglalás. Lásd a Használati útmutatóban talál. Limit elfolyó észlel prioritás osztályt #';
+$lang['qos_priomark_downstream_rules'] = 'Downstream Priority Class szabályok';
+$lang['qos_priomark_invalid_interface'] = 'Érvénytelen vagy nem konfigurált külső felület.';
+$lang['qos_priomark_not_found'] = 'Prioritási osztály szabály nem található';
+$lang['qos_priomark_upstream_rules'] = 'Upstream Priority Class szabályok';
+$lang['qos_priority'] = 'Kiemelten fontos';
+$lang['qos_ramp'] = 'Rámpa';
+$lang['qos_rate_to_quantum_auto'] = 'Automatikus Rate-to-Quantum';
+$lang['qos_rate_to_quantum'] = 'Rate-to-Quantum';
+$lang['qos_reset'] = 'Visszaállítás';
+$lang['qos_source'] = 'Forrás';
+$lang['qos_protocol_must_be_specified_using_ports'] = 'Jegyzőkönyv kell állítani a TCP vagy UDP portok használata esetén.';
