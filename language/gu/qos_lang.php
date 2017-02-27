d47228a00f7e7a3e727931da23e85e61673aea63
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:18:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/qos_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['qos_add_priomark_downstream_rule'] = 'ડાઉનસ્ટ્રીમ પ્રાધાન્યતા વર્ગ નિયમ ઉમેરો';
+$lang['qos_add_priomark_upstream_rule'] = 'અપસ્ટ્રીમ પ્રાધાન્યતા વર્ગ નિયમ ઉમેરો';
+$lang['qos_any'] = 'કોઈપણ';
+$lang['qos_app_description'] = 'બેન્ડવીડ્થ અને QoS વ્યવસ્થાપક એપ્લિકેશન નેટવર્ક લક્ષણ કે સંચાલકો ઈન્ટરનેટ ટ્રાફિક ચોક્કસ પ્રકારના પ્રાથમિકતા માટે પરવાનગી આપે છે. QoS સક્રિય કરી રહ્યા શક્યતા છે કે જે કોઈપણ સમયે, એક વપરાશકર્તા અથવા ઉપકરણ ઉપલબ્ધ બેન્ડવિડ્થ saturating દ્વારા નેટવર્ક પ્રદર્શન ઘટતાં શકે ઘટે છે.';
+$lang['qos_app_name'] = 'બેન્ડવીડ્થ અને QoS વ્યવસ્થાપક';
+$lang['qos_destination'] = 'લક્ષ્યસ્થાન';
+$lang['qos_direction'] = 'દિશા';
+$lang['qos_edit_priomark_downstream_rule'] = 'ફેરફાર કરો ડાઉનસ્ટ્રીમ પ્રાધાન્યતા વર્ગ નિયમ';
+$lang['qos_edit_priomark_upstream_rule'] = 'ફેરફાર કરો અપસ્ટ્રીમ પ્રાધાન્યતા વર્ગ નિયમ';
+$lang['qos_enable_all'] = 'બધા સક્ષમ';
+$lang['qos_engine_disable'] = 'અક્ષમ કરો એન્જિન';
+$lang['qos_engine_enable'] = 'એન્જિન સક્ષમ';
+$lang['qos_equalize'] = 'સરખું';
+$lang['qos_interface_add_title'] = 'બાહ્ય ઈન્ટરફેસ રૂપરેખાંકન ઉમેરો';
+$lang['qos_interface_edit_title'] = 'સંપાદિત કરો બાહ્ય ઈન્ટરફેસ રૂપરેખાંકન';
+$lang['qos_interface_speed_not_set'] = 'આ ઈન્ટરફેસ માટે ક્યાં તો નથી અપસ્ટ્રીમ અથવા હેઠવાસ સમૂહ છે. QoS એન્જિન ઉમેરતા પહેલા તેમાં ઈન્ટરફેસ પર ઝડપ ટેસ્ટ ચલાવવા કરો.';
+$lang['qos_invalid_direction'] = 'અમાન્ય દિશામાં.';
+$lang['qos_invalid_limit_value'] = 'અમાન્ય મર્યાદા મૂલ્ય.';
+$lang['qos_invalid_r2q'] = 'અમાન્ય r2q મૂલ્ય.';
+$lang['qos_invalid_reservation_value'] = 'અમાન્ય આરક્ષણ નિયમ.';
+$lang['qos_name'] = 'નામ';
+$lang['qos_not'] = 'નથી';
+$lang['qos_prioclass_limit_underflow'] = 'એક અગ્રતા વર્ગ મર્યાદા અનુરૂપ બેન્ડવિડ્થ આરક્ષણ કરતાં ઓછી કિંમત સેટ કરી શકો છો. વધુ માહિતી માટે વપરાશકર્તા માર્ગદર્શિકા જુઓ. મર્યાદા underflow અગ્રતા વર્ગ # માટે શોધાયેલ';
+$lang['qos_priomark_downstream_rules'] = 'ડાઉનસ્ટ્રીમ પ્રાધાન્યતા વર્ગ નિયમો';
+$lang['qos_priomark_invalid_interface'] = 'અમાન્ય અથવા unconfigured બાહ્ય ઈન્ટરફેસ.';
+$lang['qos_priomark_not_found'] = 'પ્રાધાન્યતા વર્ગ નિયમ મળ્યો નથી';
+$lang['qos_priomark_upstream_rules'] = 'અપસ્ટ્રીમ પ્રાધાન્યતા વર્ગ નિયમો';
+$lang['qos_priority'] = 'પ્રાથમિકતા';
+$lang['qos_ramp'] = 'રેમ્પ';
+$lang['qos_rate_to_quantum_auto'] = 'સ્વયંચાલિત દર-થી-ક્વોન્ટમ';
+$lang['qos_rate_to_quantum'] = 'દર-થી-ક્વોન્ટમ';
+$lang['qos_reset'] = 'રીસેટ';
+$lang['qos_source'] = 'સોર્સ';
+$lang['qos_protocol_must_be_specified_using_ports'] = 'જો બંદરોનો ઉપયોગ કરીને અને પ્રોટોકોલ TCP અથવા UDP સુયોજિત થયેલ હોવું જ જોઈએ.';
