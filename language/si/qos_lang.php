c784afc457b54f7ff64c8368185b52a30142fd63
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:24:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/qos_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['qos_add_priomark_downstream_rule'] = 'ගඟ පහළ ප්රමුඛ පන්ති පාලනය එකතු කරන්න';
+$lang['qos_add_priomark_upstream_rule'] = 'ගඟ ප්රමුඛ පන්ති පාලනය එකතු කරන්න';
+$lang['qos_any'] = 'ඕනෑම';
+$lang['qos_app_description'] = 'මෙම කලාප පළල හා QoS කළමනාකරු යෙදුම පරිපාලකයන් අන්තර්ජාල තදබදය ඇතැම් වර්ගවල ප්රමුඛත්වය කිරීමට ඉඩ සලසා දෙන ලෙස ජාලය ලක්ෂණයකි. QoS සක්රීය කිරීම ඕනෑම අවස්ථාවක, තනි පරිශීලක හෝ පරිශීලක උපාංගය ලබාගත හැකි කලාප පළල සන්තෘප්ත කිරීම මගින් ජාලයේ කාර්යසාධනය හායනය හැකි බව සම්භාවිතාව අඩු වේ.';
+$lang['qos_app_name'] = 'කලාප පළල හා QoS කළමනාකරු';
+$lang['qos_destination'] = 'ගමනාන්තය';
+$lang['qos_direction'] = 'අධ්යක්ෂණය';
+$lang['qos_edit_priomark_downstream_rule'] = 'ගඟ පහළ ප්රමුඛ පන්ති පාලනය සංස්කරණය කරන්න';
+$lang['qos_edit_priomark_upstream_rule'] = 'ගඟ ප්රමුඛ පන්ති පාලනය සංස්කරණය කරන්න';
+$lang['qos_enable_all'] = 'සියලු සක්රිය කරන්න';
+$lang['qos_engine_disable'] = 'ආබාධිත එන්ජින්';
+$lang['qos_engine_enable'] = 'එන්ජින් සබල කරන්න';
+$lang['qos_equalize'] = 'සමානකරනය';
+$lang['qos_interface_add_title'] = 'විදේශ අතුරුමුහුණත මානකරනය එකතු කරන්න';
+$lang['qos_interface_edit_title'] = 'සංස්කරණය කරන්න, විදේශ අතුරුමුහුණත මානකරනය';
+$lang['qos_interface_speed_not_set'] = 'මෙම අතුරුමුහුණත සඳහා එක්කෝ ඉහළින් උමාඔයේ හෝ පහළ ද්රෝණි නැහැ කට්ටලයක් ඇත. මෙම QoS එන්ජිම එය එකතු කිරීමට පෙර අතුරු මුහුණත මත ස්පීඩ් ටෙස්ට් ධාවනය කරන්න.';
+$lang['qos_invalid_direction'] = 'වලංගු නොවන දිශාව.';
+$lang['qos_invalid_limit_value'] = 'වලංගු නොවන සීමාව වටිනාකම.';
+$lang['qos_invalid_r2q'] = 'වලංගු නොවන r2q වටිනාකම.';
+$lang['qos_invalid_reservation_value'] = 'වලංගු නොවන වෙන් නීතිය.';
+$lang['qos_name'] = 'නම';
+$lang['qos_not'] = 'නැත';
+$lang['qos_prioclass_limit_underflow'] = 'ඒ ප්රමුඛත්වය පන්තිය සීමාව අදාළ කලාප පළල වෙන් වඩා අඩු අගයක් ලෙස සකසා ගත නොහැක. වැඩි වැඩියෙන් තොරතුරු සඳහා පරිශීලක මාර්ගෝපදේශය බලන්න. සීමාව underflow ප්රමුඛත්වය පන්තිය # සඳහා හඳුනාගත්';
+$lang['qos_priomark_downstream_rules'] = 'ගඟ පහළ ප්රමුඛ ය රීති';
+$lang['qos_priomark_invalid_interface'] = 'සාවද්ය හෝ unconfigured බාහිර මුහුණතක්.';
+$lang['qos_priomark_not_found'] = 'ප්රමුඛ පන්ති පාලනය සොයාගත නොහැකි';
+$lang['qos_priomark_upstream_rules'] = 'ඉහළින් උමාඔයේ ප්රමුඛ ය රීති';
+$lang['qos_priority'] = 'ප්රමුඛ';
+$lang['qos_ramp'] = 'බෑවුම';
+$lang['qos_rate_to_quantum_auto'] = 'ස්වයංක්රීය අනුපාතිකය-කිරීමට ක්වොන්ටම්';
+$lang['qos_rate_to_quantum'] = 'අනුපාතය-කිරීමට ක්වොන්ටම්';
+$lang['qos_reset'] = 'නැවත සකසන්න';
+$lang['qos_source'] = 'මූලාශ්රය';
+$lang['qos_protocol_must_be_specified_using_ports'] = 'ප්රොටොකෝලය වරාය භාවිතා කරන්නේ නම් TCP හෝ UDP සකසා ගත යුතු ය.';
