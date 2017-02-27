3dd74ca6d40745a89b9a0f7052e6b351d90e03b5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:18:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gl/qos_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['qos_add_priomark_downstream_rule'] = 'Agregar regra Clase Downstream Priority';
+$lang['qos_add_priomark_upstream_rule'] = 'Agregar regra Clase Prioridade Upstream';
+$lang['qos_any'] = 'calquera';
+$lang['qos_app_description'] = 'A aplicación ancho de banda e QoS Manager é un recurso de rede que permite aos administradores de priorizar determinados tipos de tráfico de Internet. Habilitando QoS diminúe a probabilidade de que, en determinado momento, un único usuario ou dispositivo degradar o desempeño da rede por saturación do ancho de banda dispoñible.';
+$lang['qos_app_name'] = 'Ancho de banda e QoS Xestor';
+$lang['qos_destination'] = 'destino';
+$lang['qos_direction'] = 'dirección';
+$lang['qos_edit_priomark_downstream_rule'] = 'Editar dominio de clase Downstream Priority';
+$lang['qos_edit_priomark_upstream_rule'] = 'Editar dominio de clase Prioridade Upstream';
+$lang['qos_enable_all'] = 'activar todo';
+$lang['qos_engine_disable'] = 'motor Disable';
+$lang['qos_engine_enable'] = 'activar Motor';
+$lang['qos_equalize'] = 'igualar';
+$lang['qos_interface_add_title'] = 'Agregar Configuración de Interface externa';
+$lang['qos_interface_edit_title'] = 'Editar configuración Interface externa';
+$lang['qos_interface_speed_not_set'] = 'Hai ou non set a anterior ou posterior a esta interface. Por favor, leve a cabo unha proba de velocidade na interfaz antes de engadir-la ao motor de QoS.';
+$lang['qos_invalid_direction'] = 'dirección válido.';
+$lang['qos_invalid_limit_value'] = 'valor límite válido.';
+$lang['qos_invalid_r2q'] = 'valor r2q válido.';
+$lang['qos_invalid_reservation_value'] = 'regra de reserva válido.';
+$lang['qos_name'] = 'nome';
+$lang['qos_not'] = 'Non';
+$lang['qos_prioclass_limit_underflow'] = 'Un límite de clase de prioridade non pode ser definido como un valor menor que a reserva de banda correspondente. Consulte a Guía de usuario para obter máis información. Límite underflow detectado por clase de prioridade #';
+$lang['qos_priomark_downstream_rules'] = 'Normas de clase participada prioritarias';
+$lang['qos_priomark_invalid_interface'] = 'interface externa non é válida ou non configurado.';
+$lang['qos_priomark_not_found'] = 'regra de prioridade non atopou';
+$lang['qos_priomark_upstream_rules'] = 'Normas de clase Upstream prioritarias';
+$lang['qos_priority'] = 'prioridade';
+$lang['qos_ramp'] = 'rampa';
+$lang['qos_rate_to_quantum_auto'] = 'Automatic Rate-to-Quantum';
+$lang['qos_rate_to_quantum'] = 'Rate-to-Quantum';
+$lang['qos_reset'] = 'restablecer';
+$lang['qos_source'] = 'fonte';
+$lang['qos_protocol_must_be_specified_using_ports'] = 'Protocolo debe ser definido como TCP ou UDP se está a usar portas.';
