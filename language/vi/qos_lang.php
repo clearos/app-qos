c46fde33bf3413bcb09ad5475cc540abb432737d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:27:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/qos_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['qos_add_priomark_downstream_rule'] = 'Thêm ưu tiên hạ lưu lớp Rule';
+$lang['qos_add_priomark_upstream_rule'] = 'Thêm Upstream ưu tiên lớp Rule';
+$lang['qos_any'] = 'Bất kì';
+$lang['qos_app_description'] = 'Các ứng dụng băng thông và QoS Manager là một tính năng mạng cho phép các quản trị viên để ưu tiên một số loại lưu lượng Internet. Việc kích hoạt QoS giảm khả năng là tại bất kỳ thời điểm nào, một người dùng hoặc thiết bị có thể làm suy giảm hiệu suất mạng bằng cách bão hòa băng thông có sẵn.';
+$lang['qos_app_name'] = 'Băng thông và QoS quản lý';
+$lang['qos_destination'] = 'Nơi đến';
+$lang['qos_direction'] = 'Phương hướng';
+$lang['qos_edit_priomark_downstream_rule'] = 'Chỉnh sửa Ưu tiên hạ lưu lớp Rule';
+$lang['qos_edit_priomark_upstream_rule'] = 'Chỉnh sửa Upstream ưu tiên lớp Rule';
+$lang['qos_enable_all'] = 'Cho phép tất cả';
+$lang['qos_engine_disable'] = 'Vô hiệu hóa động cơ';
+$lang['qos_engine_enable'] = 'Kích hoạt tính năng động cơ';
+$lang['qos_equalize'] = 'cân nhau';
+$lang['qos_interface_add_title'] = 'Thêm giao diện Cấu hình bên ngoài';
+$lang['qos_interface_edit_title'] = 'Chỉnh sửa giao diện Cấu hình bên ngoài';
+$lang['qos_interface_speed_not_set'] = 'Hoặc không thượng nguồn hay hạ lưu các thiết lập cho giao diện này. Xin vui lòng chạy một thử nghiệm tốc độ trên giao diện trước khi thêm nó vào cơ QoS.';
+$lang['qos_invalid_direction'] = 'hướng không hợp lệ.';
+$lang['qos_invalid_limit_value'] = 'giá trị giới hạn không hợp lệ.';
+$lang['qos_invalid_r2q'] = 'giá trị r2q không hợp lệ.';
+$lang['qos_invalid_reservation_value'] = 'quy tắc phòng không hợp lệ.';
+$lang['qos_name'] = 'Tên';
+$lang['qos_not'] = 'không phải';
+$lang['qos_prioclass_limit_underflow'] = 'Một giới hạn lớp ưu tiên không thể được thiết lập để một giá trị ít hơn so với đặt phòng băng thông tương ứng. Xem Hướng dẫn sử dụng để biết thêm thông tin. Giới hạn phát hiện underflow cho lớp ưu tiên #';
+$lang['qos_priomark_downstream_rules'] = 'Nội quy lớp ưu tiên hạ lưu';
+$lang['qos_priomark_invalid_interface'] = 'giao diện bên ngoài không hợp lệ hoặc chưa được cấu hình.';
+$lang['qos_priomark_not_found'] = 'Ưu tiên quy tắc lớp học không tìm thấy';
+$lang['qos_priomark_upstream_rules'] = 'Nội quy lớp ưu tiên ngược dòng';
+$lang['qos_priority'] = 'Sự ưu tiên';
+$lang['qos_ramp'] = 'Giốc';
+$lang['qos_rate_to_quantum_auto'] = 'Tự động Tỷ-to-Quantum';
+$lang['qos_rate_to_quantum'] = 'Đánh giá-to-Quantum';
+$lang['qos_reset'] = 'Thiết lập lại';
+$lang['qos_source'] = 'nguồn';
+$lang['qos_protocol_must_be_specified_using_ports'] = 'Nghị định thư phải được thiết lập TCP hoặc UDP nếu sử dụng cổng.';
