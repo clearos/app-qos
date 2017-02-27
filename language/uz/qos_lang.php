44b39e328f9aaee3adb63fea4b739e0b0c2477d8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:27:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/qos_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['qos_add_priomark_downstream_rule'] = 'Downstream ustuvor Class qoida qo&#39;shish';
+$lang['qos_add_priomark_upstream_rule'] = 'Upstream ustuvor Class qoida qo&#39;shish';
+$lang['qos_any'] = 'har qanday';
+$lang['qos_app_description'] = 'Bandwidth va QoS menejeri ilova ma&#39;murlari Internet trafik ayrim turlarini birinchi o&#39;ringa imkonini beruvchi tarmoq xususiyati hisoblanadi. QoS qulay shart har qanday vaqtda, bir foydalanuvchi yoki qurilma mumkin tarmoqli kengligi to&#39;ldirish tomonidan tarmoq ishlashini mumkin mumkin ehtimolini kamaytiradi.';
+$lang['qos_app_name'] = 'Bandwidth va QoS menejeri';
+$lang['qos_destination'] = 'manzil';
+$lang['qos_direction'] = 'yo&#39;nalish';
+$lang['qos_edit_priomark_downstream_rule'] = 'Downstream ustuvor Class qoida tahrirlash';
+$lang['qos_edit_priomark_upstream_rule'] = 'Upstream ustuvor Class qoida tahrirlash';
+$lang['qos_enable_all'] = 'Hammasini yoqish';
+$lang['qos_engine_disable'] = 'o&#39;chirish Engine';
+$lang['qos_engine_enable'] = 'Engine yoqish';
+$lang['qos_equalize'] = 'hisobni tenglashtirish';
+$lang['qos_interface_add_title'] = 'Tashqi Interface Konfiguratsiya qo&#39;shish';
+$lang['qos_interface_edit_title'] = 'Edit Tashqi Interface Configuration';
+$lang['qos_interface_speed_not_set'] = 'Bu interfeys uchun emas upstream yoki pastga qarab o&#39;rnatilgan yo yo&#39;q. QoS mexanizmi qo&#39;shishdan avval, interfeysi bir Speed ​​sinov ishga tushirish.';
+$lang['qos_invalid_direction'] = 'Noto&#39;g&#39;ri yo&#39;nalishi.';
+$lang['qos_invalid_limit_value'] = 'Noto&#39;g&#39;ri chegarasi qiymati.';
+$lang['qos_invalid_r2q'] = 'Noto&#39;g&#39;ri r2q qiymati.';
+$lang['qos_invalid_reservation_value'] = 'Noto&#39;g&#39;ri saqlash qoida.';
+$lang['qos_name'] = 'ism';
+$lang['qos_not'] = 'yo&#39;q';
+$lang['qos_prioclass_limit_underflow'] = 'A ustuvor sinf chegarasi tegishli bandwidth zaxiraga kam, bir qiymatiga o&#39;rnatilgan bo&#39;lishi mumkin emas. Qo&#39;shimcha ma&#39;lumot olish uchun Foydalanuvchining ko&#39;rsatma. Limit underflow ustuvor sinf # aniqlangan';
+$lang['qos_priomark_downstream_rules'] = 'Downstream ustuvor Class qoidalari';
+$lang['qos_priomark_invalid_interface'] = 'Noto&#39;g&#39;ri yoki Kontanklaringizni tashqi interfeysi.';
+$lang['qos_priomark_not_found'] = 'Ustuvor sinf qoida topilmadi';
+$lang['qos_priomark_upstream_rules'] = 'Upstream ustuvor Class qoidalari';
+$lang['qos_priority'] = 'ustuvor';
+$lang['qos_ramp'] = 'Ramp';
+$lang['qos_rate_to_quantum_auto'] = 'Avtomatik darajasi-to-Quantum';
+$lang['qos_rate_to_quantum'] = 'Rate-to-Quantum';
+$lang['qos_reset'] = 'o&#39;rnatish';
+$lang['qos_source'] = 'manba';
+$lang['qos_protocol_must_be_specified_using_ports'] = 'portlar foydalanayotgan bo&#39;lsangiz bayonnomasi TCP yoki UDP o&#39;rnatilgan bo&#39;lishi kerak.';
