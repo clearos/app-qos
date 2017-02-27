a4d19eec5699db3e415b52e9aad02d328712366b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:15:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/qos_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['qos_add_priomark_downstream_rule'] = 'Ավելացնել Հոսանքն ի վար Առաջնայնությունը դասակարգային տիրապետության';
+$lang['qos_add_priomark_upstream_rule'] = 'Ավելացնել Հոսանքն ի վեր Առաջնայնությունը դասակարգային տիրապետության';
+$lang['qos_any'] = 'Ցանկացած';
+$lang['qos_app_description'] = 'Թողունակությունը եւ QoS մենեջեր ծրագիրը մի ցանց առանձնահատկությունն է, որ թույլ է տալիս ադմինիստրատորներին առաջնահերթ որոշակի տեսակի ինտերնետ-թրաֆիկի. Հնարավորություն տալով QoS նվազեցնում է հավանականությունը, որ ցանկացած պահի, մեկ օգտագործողի կամ սարքը կարող է ստորացնել ցանց կատարումը, հագեցնելով մատչելի թողունակությունը:';
+$lang['qos_app_name'] = 'Թողունակություն եւ QoS մենեջեր';
+$lang['qos_destination'] = 'նշանակման վայր';
+$lang['qos_direction'] = 'ուղղություն';
+$lang['qos_edit_priomark_downstream_rule'] = 'Խմբագրել Հոսանքն ի վար Առաջնայնությունը դասակարգային տիրապետության';
+$lang['qos_edit_priomark_upstream_rule'] = 'Խմբագրել Հոսանքն ի վեր Առաջնայնությունը դասակարգային տիրապետության';
+$lang['qos_enable_all'] = 'Միացնել բոլորը';
+$lang['qos_engine_disable'] = 'անջատել Engine';
+$lang['qos_engine_enable'] = 'Միացնել Engine';
+$lang['qos_equalize'] = 'հավասարեցնել';
+$lang['qos_interface_add_title'] = 'Էջանշան արտաքին ինտերֆեյսի կարգավորում';
+$lang['qos_interface_edit_title'] = 'Խմբագրել Արտաքին Ինտերֆեյս Կազմաձեւման';
+$lang['qos_interface_speed_not_set'] = 'Կա կամ չի հոսանքին հակառակ, կամ հոսանքն ի վար դրված է այս ինտերֆեյսի. Խնդրում ենք վարում մի Speed ​​Test վրա միջերեսի ավելացնելուց առաջ այն QoS շարժիչը:';
+$lang['qos_invalid_direction'] = 'Անվավեր ուղղությամբ:';
+$lang['qos_invalid_limit_value'] = 'Անվավեր սահմանը արժեք:';
+$lang['qos_invalid_r2q'] = 'Անվավեր r2q արժեքը:';
+$lang['qos_invalid_reservation_value'] = 'Անվավեր վերապահումը կանոն.';
+$lang['qos_name'] = 'անուն';
+$lang['qos_not'] = 'ոչ';
+$lang['qos_prioclass_limit_underflow'] = 'Գերակա դասի սահմանափակում չի կարող սահմանվել մի արժեք պակաս համապատասխան թողունակության վերապահման: Տես օգտագործողի ուղեցույցը: Լրացուցիչ տեղեկությունների համար. Սահմանափակել underflow հայտնաբերվել առաջնահերթություն դասի #';
+$lang['qos_priomark_downstream_rules'] = 'Հոսանքն ի վար Priority Class Օրենքները';
+$lang['qos_priomark_invalid_interface'] = 'Սխալ կամ Չկազմաձեւված արտաքին ինտերֆեյսի.';
+$lang['qos_priomark_not_found'] = 'Priority կարգի կանոն չի գտնվել';
+$lang['qos_priomark_upstream_rules'] = 'Հոսանքն ի վեր Priority Class Օրենքները';
+$lang['qos_priority'] = 'առաջնություն';
+$lang['qos_ramp'] = 'կորզում';
+$lang['qos_rate_to_quantum_auto'] = 'Ավտոմատ Գնահատել-to-Քվանտ';
+$lang['qos_rate_to_quantum'] = 'Rate-to-Քվանտ';
+$lang['qos_reset'] = 'տեղավորել';
+$lang['qos_source'] = 'աղբյուր';
+$lang['qos_protocol_must_be_specified_using_ports'] = 'Արձանագրություն պետք է սահմանել TCP կամ UDP եթե օգտագործելով նավահանգիստները:';
