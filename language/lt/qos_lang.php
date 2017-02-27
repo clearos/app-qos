2f024fd7688dac299e3b8d0499baf585d18097b9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:21:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/qos_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['qos_add_priomark_downstream_rule'] = 'Pridėti Pasroviui prioriteto klasė taisyklė';
+$lang['qos_add_priomark_upstream_rule'] = 'Pridėti Upstream prioriteto klasė taisyklė';
+$lang['qos_any'] = 'bet koks';
+$lang['qos_app_description'] = 'Pralaidumo ir QoS direktorius programa yra tinklo funkcija, kuri leidžia administratoriams prioritetą tam tikrų rūšių interneto srautą. Įjungus QoS sumažina tikimybę, kad bet kuriuo metu, vienas vartotojas ar įrenginys gali pabloginti tinklo našumą prisotins pralaidumą.';
+$lang['qos_app_name'] = 'Pralaidumą ir QoS direktorius';
+$lang['qos_destination'] = 'Kelionės tikslas';
+$lang['qos_direction'] = 'Kryptis';
+$lang['qos_edit_priomark_downstream_rule'] = 'Redaguoti Pasroviui prioriteto klasė taisyklė';
+$lang['qos_edit_priomark_upstream_rule'] = 'Redaguoti Upstream prioriteto klasė taisyklė';
+$lang['qos_enable_all'] = 'kad visi';
+$lang['qos_engine_disable'] = 'Išjungti Variklis';
+$lang['qos_engine_enable'] = 'Įjungti sistema';
+$lang['qos_equalize'] = 'suvienodinti';
+$lang['qos_interface_add_title'] = 'Pridėti išorinės sąsajos konfigūraciją';
+$lang['qos_interface_edit_title'] = 'Redaguoti išorinės sąsajos konfigūracijos';
+$lang['qos_interface_speed_not_set'] = 'Yra arba nėra žemupio ar aukštupio rinkinys šią sąsają. prieš pridedant jį prie QoS variklio Prašome paleisti Speed ​​Test apie sąsają.';
+$lang['qos_invalid_direction'] = 'Neteisingas kryptimi.';
+$lang['qos_invalid_limit_value'] = 'Neteisingas ribinė vertė.';
+$lang['qos_invalid_r2q'] = 'Neteisingas r2q vertė.';
+$lang['qos_invalid_reservation_value'] = 'Neteisingas rezervavimas taisyklė.';
+$lang['qos_name'] = 'vardas';
+$lang['qos_not'] = 'ne';
+$lang['qos_prioclass_limit_underflow'] = 'Prioritetas klasės riba negali būti nustatyta vertė mažesnė nei atitinkamu pralaidumo juostas. Vartotojo vadove rasite daugiau informacijos. Riba Perkrautas apačios aptikta prioriteto klasė #';
+$lang['qos_priomark_downstream_rules'] = 'Pasroviui prioriteto klasė Taisyklės';
+$lang['qos_priomark_invalid_interface'] = 'Neteisingas arba nieskonfigurowanej išorinės sąsajos.';
+$lang['qos_priomark_not_found'] = 'Prioriteto klasė taisyklė nerastas';
+$lang['qos_priomark_upstream_rules'] = 'Prieš prioriteto klasė Taisyklės';
+$lang['qos_priority'] = 'Prioritetas';
+$lang['qos_ramp'] = 'rampa';
+$lang['qos_rate_to_quantum_auto'] = 'Automatinis rodiklis, kad Kvantas';
+$lang['qos_rate_to_quantum'] = 'Norma iki Quantum';
+$lang['qos_reset'] = 'Atstatyti';
+$lang['qos_source'] = 'šaltinis';
+$lang['qos_protocol_must_be_specified_using_ports'] = 'Protokolas turi būti nustatytas į TCP arba UDP jei naudojate uostus.';
