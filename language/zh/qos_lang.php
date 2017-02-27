351f627ef46086b1e2e9b65019ef6c36ef322e0e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:16:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/qos_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['qos_add_priomark_downstream_rule'] = '添加下行优先级规则';
+$lang['qos_add_priomark_upstream_rule'] = '添加上行优先级规则';
+$lang['qos_any'] = '任何';
+$lang['qos_app_description'] = '带宽和QoS管理器应用程序是一种网络功能，允许管理员对某些类型的互联网流量的优先级。 有利的QoS降低，在任何给定时间，一个用户或设备可能被饱和可用带宽降低网络性能的可能性。';
+$lang['qos_app_name'] = '带宽和QoS管理器';
+$lang['qos_destination'] = '目的地';
+$lang['qos_direction'] = '方向';
+$lang['qos_edit_priomark_downstream_rule'] = '编辑下游优先级规则';
+$lang['qos_edit_priomark_upstream_rule'] = '编辑上游优先级规则';
+$lang['qos_enable_all'] = '全部启用';
+$lang['qos_engine_disable'] = '禁用引擎';
+$lang['qos_engine_enable'] = '启用引擎';
+$lang['qos_equalize'] = '均衡';
+$lang['qos_interface_add_title'] = '添加外部接口配置';
+$lang['qos_interface_edit_title'] = '编辑外部接口配置';
+$lang['qos_interface_speed_not_set'] = '有这种接口要么没有上游或下游集。 请将它添加到QoS引擎之前运行界面速度测试。';
+$lang['qos_invalid_direction'] = '无效的方向。';
+$lang['qos_invalid_limit_value'] = '无效的限值。';
+$lang['qos_invalid_r2q'] = '无效r2q值。';
+$lang['qos_invalid_reservation_value'] = '无效的保留规则。';
+$lang['qos_name'] = '名称';
+$lang['qos_not'] = '不';
+$lang['qos_prioclass_limit_underflow'] = '一个优先级的限制不能设定为小于相应的带宽预留的值。 请参阅用户手册以获取更多信息。 对于优先级＃检出限溢';
+$lang['qos_priomark_downstream_rules'] = '下游优先级规则';
+$lang['qos_priomark_invalid_interface'] = '无效或未配置的外部接口。';
+$lang['qos_priomark_not_found'] = '优先级规则未找到';
+$lang['qos_priomark_upstream_rules'] = '上游优先级规则';
+$lang['qos_priority'] = '优先';
+$lang['qos_ramp'] = '斜坡';
+$lang['qos_rate_to_quantum_auto'] = '自动速率对量子';
+$lang['qos_rate_to_quantum'] = '率到量子';
+$lang['qos_reset'] = '重启';
+$lang['qos_source'] = '资源';
+$lang['qos_protocol_must_be_specified_using_ports'] = '协议必须如使用的端口设置为TCP或UDP。';
