2fddc03c9869be39a611776bd097191b816b7723
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:16:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/qos_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['qos_add_priomark_downstream_rule'] = 'မြစ်အောက်ပိုင်းဦးစားပေး Class ကိုနည်းဥပဒေ Add';
+$lang['qos_add_priomark_upstream_rule'] = 'မြစ်ညာဦးစားပေး Class ကိုနည်းဥပဒေ Add';
+$lang['qos_any'] = 'မည်သည့်';
+$lang['qos_app_description'] = 'အဆိုပါလှိုင်းအကျယ်များနှင့် QoS Manager ကို app ကိုအုပ်ချုပ်ရေးမှူးများကအင်တာနက် traffic ကိုအချို့အမျိုးအစားများကိုဦးစားပေးခွင့်ပြုကွန်ယက် feature တစ်ခုဖြစ်ပါတယ်။ QoS ကို Enable ဆိုပေးထားအချိန်တွင်တစ်ခုတည်းအသုံးပြုသူသို့မဟုတ် device ကိုရရှိနိုင် bandwidth ကို saturating အားဖြင့် network တစ်ခုစွမ်းဆောင်ရည်နိမ့်စေခြင်းငှါဖြစ်နိုင်ခြေကိုလျော့နည်းစေပါသည်။';
+$lang['qos_app_name'] = 'bandwidth နဲ့ QoS Manager ကို';
+$lang['qos_destination'] = 'ခရီးလမ်းဆုံး';
+$lang['qos_direction'] = 'Direction အဖွဲ့';
+$lang['qos_edit_priomark_downstream_rule'] = 'မြစ်အောက်ပိုင်းဦးစားပေး Class ကိုနည်းဥပဒေပြင်ရန်';
+$lang['qos_edit_priomark_upstream_rule'] = 'မြစ်ညာဦးစားပေး Class ကိုနည်းဥပဒေပြင်ရန်';
+$lang['qos_enable_all'] = 'အားလုံး Enable';
+$lang['qos_engine_disable'] = 'Disable လုပ်ထားအင်ဂျင်';
+$lang['qos_engine_enable'] = 'အင်ဂျင်ပါဝါ Enable';
+$lang['qos_equalize'] = 'ညီမျှ';
+$lang['qos_interface_add_title'] = 'ပြင်ပ Interface ကို Configuration ကို Add';
+$lang['qos_interface_edit_title'] = 'Edit ကိုပြင်ပ Interface ကို Configuration';
+$lang['qos_interface_speed_not_set'] = 'ဒီ interface ကိုအဘို့အအထက်ပိုင်းသို့မဟုတ်မြစ်အောက်ပိုင်းမ set ကိုဖြစ်စေရှိပါတယ်။ အ QoS အင်ဂျင်ကဖြည့်စွက်ခြင်းမပြုမီ interface ပေါ်မှာတစ်မြန်နှုန်းစမ်းသပ်ခြင်းကို run ပေးပါ။';
+$lang['qos_invalid_direction'] = 'မှားနေသောညှနျကွားခ။';
+$lang['qos_invalid_limit_value'] = 'မှားနေသောန့်သတ်ချက်တန်ဖိုး။';
+$lang['qos_invalid_r2q'] = 'မှားနေသော r2q တန်ဖိုး။';
+$lang['qos_invalid_reservation_value'] = 'မှားနေသောကြိုတင်မှာကြားထားအုပ်ချုပ်။';
+$lang['qos_name'] = 'Name ကို';
+$lang['qos_not'] = 'မဟုတ်';
+$lang['qos_prioclass_limit_underflow'] = 'တစ်ဦးကဦးစားပေးအဆငျ့အတနျးကန့်သတ်သက်ဆိုင်တဲ့ bandwidth ကိုကြိုတင်မှာယူမှုထက်လျော့နည်းနေတဲ့တန်ဖိုး ထား. မရနိုင်ပါ။ ပိုမိုသိရှိလိုပါကများအတွက်အသုံးပြုသူလမ်းညွှန်ကိုကြည့်ပါ။ ကန့်သတ် underflow ဦးစားပေးအဆငျ့အတနျး # ဘို့တွေ့ရှိ';
+$lang['qos_priomark_downstream_rules'] = 'မြစ်အောက်ပိုင်းဦးစားပေး Class ကိုစည်းကမ်းများ';
+$lang['qos_priomark_invalid_interface'] = 'မှားနေသောသို့မဟုတ် unconfigured ပြင်ပအင်တာဖေ့စ။';
+$lang['qos_priomark_not_found'] = 'ဦးစားပေးအဆငျ့အတနျးအုပ်ချုပ်မတွေ့ရှိ';
+$lang['qos_priomark_upstream_rules'] = 'upstream ဦးစားပေး Class ကိုစည်းကမ်းများ';
+$lang['qos_priority'] = 'ဦးစားပေး';
+$lang['qos_ramp'] = 'ဆငျခွေလြှော';
+$lang['qos_rate_to_quantum_auto'] = 'အလိုအလျှောက်နှုန်း-to-Quantum';
+$lang['qos_rate_to_quantum'] = 'rate-to-Quantum';
+$lang['qos_reset'] = 'reset';
+$lang['qos_source'] = 'အရင်းအမြစ်';
+$lang['qos_protocol_must_be_specified_using_ports'] = 'ပရိုတိုကောဆိပ်ကမ်းများ အသုံးပြု. လျှင်သည် TCP သို့မဟုတ် UDP မှသတ်မှတ်ရပါမည်။';
