e969673bda19f5cb6f946fada523d6cc2b4f5e5c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:24:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pt/qos_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['qos_add_priomark_downstream_rule'] = 'Adicionar regra Classe Downstream Priority';
+$lang['qos_add_priomark_upstream_rule'] = 'Adicionar regra Classe Prioridade Upstream';
+$lang['qos_any'] = 'Qualquer';
+$lang['qos_app_description'] = 'O aplicativo Largura de Banda e QoS Manager é um recurso de rede que permite que os administradores de priorizar certos tipos de tráfego de Internet. Habilitando QoS diminui a probabilidade de que, em determinado momento, um único usuário ou dispositivo pode degradar o desempenho da rede por saturação da largura de banda disponível.';
+$lang['qos_app_name'] = 'Largura de banda e QoS Gestor';
+$lang['qos_destination'] = 'Destino';
+$lang['qos_direction'] = 'Direção';
+$lang['qos_edit_priomark_downstream_rule'] = 'Editar domínio de classe Downstream Priority';
+$lang['qos_edit_priomark_upstream_rule'] = 'Editar domínio de classe Prioridade Upstream';
+$lang['qos_enable_all'] = 'Habilitar todos';
+$lang['qos_engine_disable'] = 'Motor Disable';
+$lang['qos_engine_enable'] = 'Ativar Motor';
+$lang['qos_equalize'] = 'igualar';
+$lang['qos_interface_add_title'] = 'Adicionar Configuração de Interface externa';
+$lang['qos_interface_edit_title'] = 'Editar configuração Interface externa';
+$lang['qos_interface_speed_not_set'] = 'Há ou não set a montante ou a jusante para esta interface. Por favor, execute um teste de velocidade na interface antes de adicioná-la ao motor de QoS.';
+$lang['qos_invalid_direction'] = 'direção inválido.';
+$lang['qos_invalid_limit_value'] = 'valor-limite inválido.';
+$lang['qos_invalid_r2q'] = 'valor r2q inválido.';
+$lang['qos_invalid_reservation_value'] = 'regra de reserva inválido.';
+$lang['qos_name'] = 'Nome';
+$lang['qos_not'] = 'Não';
+$lang['qos_prioclass_limit_underflow'] = 'Um limite de classe de prioridade não pode ser definido como um valor menor do que a reserva de banda correspondente. Consulte o Guia do Usuário para obter mais informações. Limite underflow detectado por classe de prioridade #';
+$lang['qos_priomark_downstream_rules'] = 'Regras de classe jusante prioritárias';
+$lang['qos_priomark_invalid_interface'] = 'interface externa inválida ou não configurado.';
+$lang['qos_priomark_not_found'] = 'regra de prioridade não encontrado';
+$lang['qos_priomark_upstream_rules'] = 'Regras de classe Upstream prioritárias';
+$lang['qos_priority'] = 'Prioridade';
+$lang['qos_ramp'] = 'Rampa';
+$lang['qos_rate_to_quantum_auto'] = 'Automatic Rate-to-Quantum';
+$lang['qos_rate_to_quantum'] = 'Rate-to-Quantum';
+$lang['qos_reset'] = 'Restabelecer';
+$lang['qos_source'] = 'Fonte';
+$lang['qos_protocol_must_be_specified_using_ports'] = 'Protocolo deve ser definido como TCP ou UDP se estiver usando portas.';
